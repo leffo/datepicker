181e098c544346eb398c3d91fe037e7d9c5bb92e
Alexandr Yakovlev (asu) <leff@leffmkp>
2021-03-30 17:35:03 +0700

    initial datepicker coomit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+   <h1>hELLO: {{ msg }} </h1>
+   <datepicker v-model="state.date" name="dp"></datepicker>
+  </div>
+</template>
+
+
+
+<script>
+import Datetpicker from 'vuejs-datepicker';
+
+
+export default {
+  components: { Datetpicker },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      state: {
+        date: new Date(2021, 24, 10)
+      }
+    }
+  }
+}
+</script>

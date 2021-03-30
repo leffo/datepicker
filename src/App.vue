ea2abcd9d792ccb34fbf3d1f822e19988b5a2add
Alexandr Yakovlev (asu) <leff@leffmkp>
2021-03-30 17:57:08 +0700

    typeable & date

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div>
-   <h1>hELLO: {{ msg }} </h1>
-   <datepicker v-model="state.date" name="dp"></datepicker>
+   <h1>hELLO: {{ state.date }} </h1>
+   <datepicker 
+    :v-model="state.date" 
+    name="dp"
+    :language="'ru'"
+    :typeable="true"
+    format="dd.MM.yyyy"
+    >
+    </datepicker>
   </div>
 </template>
 
 
 
 <script>
-import Datetpicker from 'vuejs-datepicker';
+import Datepicker from 'vuejs-datepicker';
+import {ru} from 'vuejs-datepicker/dist/locale';
 
 
 export default {
-  components: { Datetpicker },
+  components: { Datepicker },
   name: 'app',
   data () {
     return {
+      ru: ru,
       msg: 'Welcome to Your Vue.js App',
       state: {
-        date: new Date(2021, 24, 10)
+        date: new Date(2021, 24, 10),
       }
     }
   }

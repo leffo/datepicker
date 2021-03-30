b3f5948c17590a4556fd6d4d0c74614a64555842
Alexandr Yakovlev <leffalt@gmail.com>
2021-03-30 20:34:09 +0700

    add vue2-datapicker

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
    <h1>Hello: {{ state.date }} </h1>
    <datepicker
      v-model="state.date"
-    :value="state.date"
+
     name="dp"
     :language="ru"
     :typeable="true"
@@ -11,6 +11,11 @@
     :open-date="state.date"
     >
     </datepicker>
+    <date-picker-vue2
+      v-model="time1"
+      valueType="DD.MM.YYYY"
+      :time-title-format="'DD.MM.YYYY'"
+    ></date-picker-vue2>
   </div>
 </template>
 
@@ -19,9 +24,11 @@
 <script>
 import Datepicker from 'vuejs-datepicker';
 import ru from "vuejs-datepicker/dist/locale/translations/ru";
+import DatePickerVue2 from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
 
 export default {
-  components: { Datepicker },
+  components: { Datepicker, DatePickerVue2 },
   name: 'app',
   data () {
     return {
@@ -30,7 +37,8 @@ export default {
       state: {
         // month 0...11
         date: new Date(2021, 9, 24),
-      }
+      },
+      time1: '24.10.1966',
     }
   }
 }
